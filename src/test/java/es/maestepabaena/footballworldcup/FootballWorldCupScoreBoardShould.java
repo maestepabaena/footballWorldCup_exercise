f3443eb84c293c14caf3f78caf323806fee34d5a
INT\Miguel.Estepa <miguel.estepa@solera.com>
2022-05-24 19:24:09 +0200

    Refactoring. Rename unit-test

--- src/test/java/es/maestepabaena/footballworldcup/FootballWorldCupScoreBoardShould.java
+++ src/test/java/es/maestepabaena/footballworldcup/FootballWorldCupScoreBoardShould.java
@@ -19,7 +19,7 @@ import static org.junit.Assert.assertEquals;
 public class FootballWorldCupScoreBoardShould {
 
         @Test
-        public void return_0_0_when_startGameIsCalled(){
+        public void store_matchData_when_startGameIsCalled(){
                 //given
                 FootballWorldCupScoreBoard scoreBoard = new FootballWorldCupScoreBoard();
 
